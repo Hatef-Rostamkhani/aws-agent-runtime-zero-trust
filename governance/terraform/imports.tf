20a4043ce0dae47e928312bc75d8b25ad6d8c23b
Hatef-Rostamkhani <hatef.rostamkhani@gmail.com>
2025-11-26 03:10:44 +0330

    Fix import blocks: use hardcoded values instead of variables
    Terraform import blocks do not support variables - IDs must be literal values

--- governance/terraform/imports.tf
+++ governance/terraform/imports.tf
@@ -1,28 +1,29 @@
 # Import blocks for existing resources
 # Terraform 1.5+ will automatically import these resources if they exist
+# Note: import block IDs must be literal values (variables not allowed)
 
 import {
   to = aws_dynamodb_table.policies
-  id = "${var.project_name}-governance-policies"
+  id = "agent-runtime-governance-policies"
 }
 
 import {
   to = aws_cloudwatch_log_group.governance
-  id = "/aws/lambda/${var.project_name}-governance"
+  id = "/aws/lambda/agent-runtime-governance"
 }
 
 import {
   to = aws_lambda_function.governance
-  id = "${var.project_name}-governance"
+  id = "agent-runtime-governance"
 }
 
 import {
   to = aws_iam_role.governance_lambda
-  id = "${var.project_name}-governance-lambda-role"
+  id = "agent-runtime-governance-lambda-role"
 }
 
 import {
   to = aws_iam_role_policy.governance_lambda
-  id = "${var.project_name}-governance-lambda-role:${var.project_name}-governance-lambda-policy"
+  id = "agent-runtime-governance-lambda-role:agent-runtime-governance-lambda-policy"
 }
 
