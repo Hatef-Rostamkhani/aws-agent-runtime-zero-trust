5425209fc67d4c5de7f6c4b1d3fec9a2d1108a77
Hatef-Rostamkhani <hatef.rostamkhani@gmail.com>
2025-11-26 03:13:32 +0330

    Only import resources that definitely exist
    Remove imports for Lambda function and log group that may not exist yet
    Import blocks fail if resource doesn't exist - only import known resources

--- governance/terraform/imports.tf
+++ governance/terraform/imports.tf
@@ -1,29 +1,20 @@
-# Import blocks for existing resources
-# Terraform 1.5+ will automatically import these resources if they exist
+# Import blocks for existing resources that we know exist
+# Only import resources that actually exist to avoid import failures
 # Note: import block IDs must be literal values (variables not allowed)
 
+# Import DynamoDB table (created earlier)
 import {
   to = aws_dynamodb_table.policies
   id = "agent-runtime-governance-policies"
 }
 
-import {
-  to = aws_cloudwatch_log_group.governance
-  id = "/aws/lambda/agent-runtime-governance"
-}
-
-import {
-  to = aws_lambda_function.governance
-  id = "agent-runtime-governance"
-}
-
+# Import IAM role (created by infra deployment)
 import {
   to = aws_iam_role.governance_lambda
   id = "agent-runtime-governance-lambda-role"
 }
 
-import {
-  to = aws_iam_role_policy.governance_lambda
-  id = "agent-runtime-governance-lambda-role:agent-runtime-governance-lambda-policy"
-}
+# Note: Lambda function and CloudWatch log group will be created by Terraform
+# IAM role policy will be created by Terraform
+# Only import resources that definitely exist to avoid import errors
 
